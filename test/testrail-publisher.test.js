dd2d0cbeaffdfeed1bbe5206bcbe242b5cdac1b9
joseph.hughes <joe@joehughes.org>
2018-01-23 11:36:11 +0000

    dummy commit

--- test/testrail-publisher.test.js
+++ test/testrail-publisher.test.js
@@ -6,5 +6,8 @@ describe('dummy test suite', () => {
     it('should return -1 when the value is not present', () => {
       expect([1, 2, 3].indexOf(4)).to.deep.equal(-1);
     });
+    it('should return -1 when the value is not present', () => {
+      expect([1, 2, 3].indexOf(4)).to.deep.equal(-1);
+    });
   });
 });
